3fe18a7c63e9d531221a11c1cecb0110062ac331
Maria Júlia Miosso <107309474+mjuliamiosso@users.noreply.github.com>
2025-07-14 11:16:19 -0300

    feat: search bar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from "react";
+import { IoSearchSharp } from "react-icons/io5";
 
 const SearchBar = () => {
-  return (
-    <div>SearchBar</div>
-  )
-}
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-export default SearchBar
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      buttonRef.current?.click();
+    }
+  };
+
+  return (
+    <div className="flex">
+      <input
+        className="searchShape bg-[var(--background-color)] w-[317px] h-[40px] text-base px-7 italic"
+        type="text"
+        placeholder="Search"
+        onKeyDown={handleKeyDown}
+      />
+      <button
+        ref={buttonRef}
+        className="searchButtonShape -ml-[16px] flex items-center text-xl justify-center bg-[var(--background-color)] w-[72.71px] h-[40px] hover:bg-[#282836] cursor-pointer"
+        onClick={() => {}}
+      >
+        <IoSearchSharp />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
